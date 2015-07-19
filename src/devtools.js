33d9d170921085dbceace282cdf7f4090701fafa
Callum <callum@callumj.co.uk>
2015-07-19 11:22:17 +0100

    Rename panel

--- src/devtools.js
+++ src/devtools.js
@@ -1,6 +1,4 @@
-const { elements } = chrome.devtools.panels;
-
-elements.createSidebarPane('Gradient editor', sidebar => {
+chrome.devtools.panels.elements.createSidebarPane('Gradients', sidebar => {
   sidebar.setPage('sidebar-pane.html');
   sidebar.setHeight('100ex');
 });
